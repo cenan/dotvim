6d2a1a91aa515c1ed0d2a4e918e594e09b19ea6b
cenan <me@cenanozen.com>
2013-06-19 12:21:08 +0300

    fix lion error

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+#sudo mkdir -p /usr/include/python2.7
+#sudo ln -s /System/Library/Frameworks/Python.framework/Versions/Current/include/python2.7/pyconfig.h /usr/include/python2.7/pyconfig.h
+
 ln -s ~/.vim/vimrc ~/.vimrc
 git submodule update --init
