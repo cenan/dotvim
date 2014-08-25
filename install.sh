c45532365828d864d11536027e7e6aaa49ad459a
cenan <me@cenanozen.com>
2014-08-25 23:01:22 +0300

    updates install script

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@
 #sudo ln -s /System/Library/Frameworks/Python.framework/Versions/Current/include/python2.7/pyconfig.h /usr/include/python2.7/pyconfig.h
 
 ln -s ~/.vim/vimrc ~/.vimrc
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo vim +PluginInstall +qall
